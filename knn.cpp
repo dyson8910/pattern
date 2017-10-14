1abc05ee7674d087ef3055a881396ec82ebb932e
dyson8910 <mech.b.dyson@gmail.com>
2017-10-14 12:22:40 +0900

    under construction

---
+++ knn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+  
+  ifstream ifs("iris.data");
+  if(!ofs){
+    cout << "failed opening iris.data" << endl;
+    exit(1);
+  }
+
+  
+  return 0;
+}
+
