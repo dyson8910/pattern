6920cb973fc0c8ff55cc1a73ebd56a2244c94540
dyson8910 <mech.b.dyson@gmail.com>
2017-11-10 11:37:54 +0900

    modified linear-multiple-regression.py

--- linear-multiple-regression.py
+++ linear-multiple-regression.py
@@ -8,7 +8,7 @@ data = pd.read_csv('auto-mpg.data',header=None,sep='\s+')
 data = data[data[3] != '?']
 data = np.array(data)
 for i in range(len(data)):
-    data[i,3] = float(data[i,3])
+        data[i,3] = float(data[i,3])
 
 XT = np.array([data[:,4],data[:,3]])
 XT = XT.astype(float)
@@ -28,11 +28,12 @@ x2 = np.arange(np.min(X[:,1]),np.max(X[:,1]),(np.max(X[:,1])-np.min(X[:,1]))/25.
 X1,X2 = np.meshgrid(x1,x2)
 Y =[]
 for i in range(len(X1)):
-    x = np.array([X1[i],X2[i]])
-    Y.append(np.dot(w,x))
+        x = np.array([X1[i],X2[i]])
+        Y.append(np.dot(w,x))
 ax.plot_wireframe(X1,X2,Y)
 ax.scatter(X[:,0],X[:,1],t)
 fig.show()
-fig.savefig("linear-multiple-regression.png")
+fig.savefig("result-of-linear-multiple-regression.png")
 
 fig2 = plt.figure()
+                    
